2a972d3719ce8133e6841a0a7d72663995a081ba
nattawan boonnoy <nattawan.b@kkumail.com>
2021-02-11 22:36:21 +0700

    week1

---
+++ Week1/work1.1.py
@@ -0,0 +1,9 @@
+name = input("What is your name?\n")
+surname = input("What is your surname\n")
+year = input("What is your college year\n")
+code = input("What is your Student ID\n")
+
+print("Name = %s "%name )
+print("Surname = %s "%surname)
+print("College year = %s "%year)
+print("Student ID = %s "%code)
---
+++ Week1/work1.2.py
@@ -0,0 +1,9 @@
+x = 20 
+y = 35.24
+z = 1j
+a = float(x)
+b = int(y)
+c = complex(x)
+
+print(x,y,z)
+print(a,b,c)
