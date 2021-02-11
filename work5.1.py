7faab7672b27d40d7209797e655a0c42d4a7dbd9
diamondspino <77141642+diamondspino@users.noreply.github.com>
2021-02-12 02:31:38 +0700

    week5

---
+++ work5.1.py
@@ -0,0 +1,28 @@
+print("-----แนะนำตัว-----")
+print('*'*20)
+class data :
+    def __init__(self,firstname,lastname,gender,year,department,city) :
+        self.firstname = firstname 
+        self.lastname = lastname
+        self.gender = gender
+        self.year = year
+        self.department = department
+        self.city = city
+    
+    def showdata(self) :
+        print('*'*10,"แนะนำตัว",'*'*10)
+        print("ชื่อ : ",self.firstname)
+        print("สกุล : ",self.lastname)
+        print("เพศ : ",self.gender)
+        print("ชั้นปี : ",self.year)
+        print("สาขาวิชา : ",self.department)
+        print("เมืองที่อยู่ : ",self.city)
+
+firstname = input("ชื่อ :")
+lastname = input("สกุล :")
+gender = input("เพศ :")
+year = input("ชั้นปี :")
+department = input("สาขาวิชา :")
+city = input("เมืองที่อยู่:")
+x = data(firstname,lastname,gender,year,department,city)
+x.showdata()
